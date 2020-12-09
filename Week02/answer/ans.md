7ddea9e154a0bd4bf193602a510187ecef26a4b4
LvDQ <ky935@nyu.edu>
2020-12-09 20:33:51 +0800

    add Mao's ans and minor edit my hw with TA's suggestion

---
+++ Week02/answer/ans.md
@@ -0,0 +1,13 @@
+```
+dao: 
+
+ return errors.Wrapf(code.NotFound, fmt.Sprintf("sql: %s error: %v", sql, err))
+
+
+biz:
+
+if errors.Is(err, code.NotFound} {
+
+}
+
+```
--- Week02/biz/biz.go
+++ Week02/biz/biz.go
@@ -1,15 +1,21 @@
 package biz
 
-import(
+import (
 	dao "Week02/dao"
+	"database/sql"
+
 	"github.com/pkg/errors"
 )
 
 func Biz(mode string) (*dao.RtnTables, error) {
 	var name string = "gogogo"
-	usertable,err := dao.Dao(mode)
-	if err!= nil{
-		return nil, errors.WithMessage(err, "biz() error with input: " + name)
+	usertable, err := dao.Dao(mode)
+	if err != nil {
+		//add biz level error handle, not throw out the root error
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, errors.New("0 row finded.")
+		}
+		return nil, errors.WithMessage(err, "biz() error with input: "+name)
 	}
 	return usertable, err
 }
